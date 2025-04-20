6d07d1001a8ad04b95f96265b638ef66edb57872
kickbuttowski25 <vishalsinghbisht25@gmail.com>
2025-04-20 22:40:22 +0530

    Added getShuffleAnswers function in model to shuffle the answers and created list for answers in question screen

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -1,5 +1,6 @@
 import 'package:adv_basics/answer_button.dart';
 import 'package:flutter/material.dart';
+import 'package:adv_basics/data/questions.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -11,28 +12,44 @@ class QuestionScreen extends StatefulWidget {
 }
 
 class _QuestionScreenState extends State<QuestionScreen> {
+  var currentQuestionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      currentQuestionIndex++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    final currentQuestion = questions[currentQuestionIndex];
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
-            'The Question is.....?',
-            style: TextStyle(
-              color: Colors.amber,
-              fontSize: 20,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              currentQuestion.question,
+              style: const TextStyle(
+                color: Colors.amber,
+                fontSize: 16,
+              ),
+              textAlign: TextAlign.center,
             ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          AnswerButton(onTap: () {}, answerText: "Answer 1"),
-          AnswerButton(onTap: () {}, answerText: "Answer 2"),
-          AnswerButton(onTap: () {}, answerText: "Answer 3"),
-          AnswerButton(onTap: () {}, answerText: "Answer 4"),
-        ],
+            const SizedBox(
+              height: 30,
+            ),
+            ...currentQuestion.getShuffledAnswers().map((answer) {
+              return AnswerButton(
+                onTap: answerQuestion,
+                answerText: answer,
+              );
+            }),
+          ],
+        ),
       ),
     );
   }
