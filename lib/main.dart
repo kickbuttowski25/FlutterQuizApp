c6d87f196e677cb05a24aa181ac466074d052538
kickbuttowski25 <vishalsinghbisht25@gmail.com>
2025-04-20 09:08:40 +0530

    Created Advanced Basics and a new project for flutter to learn basics

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:adv_basics/start_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.black, Colors.white],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    ),
+  );
+}
